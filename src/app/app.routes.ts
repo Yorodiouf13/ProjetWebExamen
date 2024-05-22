afb38a8328ddc594cda163f4ec11e70563c55665
Yoro <oumaryorodiouf@gmail.com>
2024-05-22 08:57:33 +0000

    valid

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,9 @@ import { AccueilGeneralComponent } from './Pages/accueil-general/accueil-general
 import { AuthentificationComponent } from './Pages/authentification-page/authentification-page.component';
 import { AccueilEtuComponent } from './Pages/Etudiant/accueil-etu/accueil-etu.component';
 import { AccueilEnsComponent } from './Pages/Enseignant/accueil-ens/accueil-ens.component';
+import { PlanningExamComponent } from './Pages/Etudiant/planning-exam/planning-exam.component';
+import { JourExamComponent } from './Pages/Etudiant/jour-exam/jour-exam.component';
+import { ResultatsComponent } from './Pages/Etudiant/resultats/resultats.component';
 
 
 export const routes: Routes = [
@@ -27,5 +30,17 @@ export const routes: Routes = [
     {
         path:'accueil-etu',
         component:AccueilEtuComponent
+    },
+    {
+        path:'planning-exam',
+        component:PlanningExamComponent
+    },
+    {
+        path:'jour-exam',
+        component:JourExamComponent
+    },
+    {
+        path:'resultats',
+        component:ResultatsComponent
     }
 ];
